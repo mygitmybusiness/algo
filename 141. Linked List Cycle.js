69d4cf159d07347d0a169f8cda1417c510cff97b
dima <37408283+dimontorpeda@users.noreply.github.com>
2024-12-25 15:21:13 +0100

    141. Linked List Cycle

---
+++ 141. Linked List Cycle.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    var first = head ? head : false;
+    var second = head && head.next ? head.next : false;
+
+    while (first && second) {
+        if (first == second) {
+            return true;
+        } else {
+            first = first.next ? first.next : false;
+            second = second.next ? second.next.next : false;
+        }
+    }
+
+    return false;
+};
