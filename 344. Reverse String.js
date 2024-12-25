a0909e0cfacd3e591d154e28972e64955730c5ca
dima <37408283+dimontorpeda@users.noreply.github.com>
2024-12-25 16:04:06 +0100

    344. Reverse String

---
+++ 344. Reverse String.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let l = 0;
+    let r = s.length - 1;
+
+    while (l < r) {
+        var tmpStorage = s[r];
+        s[r] = s[l];
+        s[l] = tmpStorage;
+
+        l++;
+        r--;
+    }
+};
