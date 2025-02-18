ae90306e13df20ef0e18a1b17de4ef81cec2ab3c
dima <37408283+dimontorpeda@users.noreply.github.com>
2025-02-18 10:05:59 +0100

    Clamp

---
+++ easy/Clamp.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} value The number to clamp.
+ * @param {number} lower The lower bound.
+ * @param {number} upper The upper bound.
+ * @returns {number} Returns the clamped number.
+ */
+export default function clamp(value, lower, upper) {
+    if (value < lower) return lower;
+    if (value > upper) return upper;
+    return value;
+  }
+  
