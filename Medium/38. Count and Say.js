68fcfa67ba30b83366a7530add9416e6ccf5fc84
dima <37408283+dimontorpeda@users.noreply.github.com>
2025-02-18 11:09:10 +0100

    38. Count and Say

---
+++ Medium/38. Count and Say.js
@@ -0,0 +1,21 @@
+function countAndSay(n: number): string {
+    let currentString = "1";
+
+    for (let i = 2; i <= n; i++) {
+        let nextString = "";
+
+        for (let j = 0, k = 0; j < currentString.length; j = k) {
+            while (
+                k < currentString.length &&
+                currentString.charAt(k) == currentString.charAt(j)
+            )
+            k++;
+
+            nextString += (k - j).toString() + currentString.charAt(j);
+        }
+        
+        currentString = nextString;
+    }
+
+    return currentString;
+}
