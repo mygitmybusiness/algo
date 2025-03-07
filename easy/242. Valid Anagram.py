4814b4c9bb903c8cc1e204dac1fac2ccb854d3f3
mygitmybusiness <37408283+mygitmybusiness@users.noreply.github.com>
2025-03-07 10:42:30 +0100

    242. Valid Anagram

---
+++ easy/242. Valid Anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        for idx in set(s):
+            if s.count(idx) != t.count(idx):
+                return False
+                
+        return True
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
