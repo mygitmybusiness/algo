73386ec7f7f3ef1ded96afa2d3805474b4e83d3b
mygitmybusiness <37408283+mygitmybusiness@users.noreply.github.com>
2025-03-11 21:50:55 +0100

    2300. Successful Pairs of Spells and Potions

---
+++ Medium/2300. Successful Pairs of Spells and Potions.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} spells
+ * @param {number[]} potions
+ * @param {number} success
+ * @return {number[]}
+ */
+var successfulPairs = function (spells, potions, success) {
+    potions.sort((a, b) => a - b);
+    const answer = [];
+
+    const m = potions.length;
+    const maxPotion = potions[m - 1];
+
+    for (const spell of spells) {
+        const minPotion = Math.ceil(success / spell);
+
+        if (minPotion > maxPotion) {
+            answer.push(0);
+            continue;
+        }
+
+        const index = lowerBound(potions, minPotion);
+        answer.push(m - index);
+    }
+
+    return answer;
+};
+
+function lowerBound(arr, key) {
+    let low = 0, high = arr.length;
+
+    while (low < high) {
+        const mid = Math.floor((low + high) / 2);
+
+        if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    
+    return low;
+};
