214b54086c5a01cd1dc9f6bb48e2f1ef8107b491
mygitmybusiness <37408283+mygitmybusiness@users.noreply.github.com>
2025-03-11 09:58:54 +0100

    basic 2d matrix bfs algo

---
+++ algos/Matrix_BFS.js
@@ -0,0 +1,44 @@
+const matrixBFS = (matrix, startRow = 0, startCol = 0) => {
+    let rows = grid.length;
+    let cols = grid[0].length;
+
+    const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
+    visited[startRow][startCol] = true;
+
+    // up, down, left, right
+    const directions = [
+        { dr: -1, dc: 0 },
+        { dr: 1, dc: 0 },
+        { dr: 0, dc: -1 },
+        { dr: 0, dc: 1 },
+    ];
+
+    const queue = [];
+    queue.push({ row: startRow, col: startCol });
+
+    const visitedOrder = [];
+
+    while (queue.length > 0) {
+        // deque
+        const { row, col } = queue.shift();
+        visitedOrder.push([row, col]);
+
+        // traverse neighbours
+        for (let { dr, dc } of directions) {
+            const newRow = row + dr;
+            const newCol = col + dc;
+
+            // check boundaries and check if not blocked
+            if (
+                newRow >= 0 && newRow < rows &&
+                newCol >= 0 && newCol < cols &&
+                !visited[newRow][newCol]   // not already visited
+            ) {
+                visited[newRow][newCol] = true;  // mark visited
+                queue.push({ row: newRow, col: newCol });
+            }
+        }
+    }
+
+    console.log("visited order: ", visitedOrder);
+}
