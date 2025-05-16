503b0b0f98534bedb63db8993d61020bd232dfee
dima <37408283+dimontorpeda@users.noreply.github.com>
2025-05-16 10:00:00 +0200

    Add data model documentation

---
+++ Medium/recursive subsets.js
@@ -0,0 +1,22 @@
+function combinations(n, k) {
+    let result = [];
+  
+    function traverse(arr, depth) {
+      if (arr.length === k) {
+        result.push(arr);
+        return;
+      }
+      
+      if (depth > n) {      
+        return;
+      }
+  
+      traverse(arr, depth + 1);  
+      traverse(arr.concat(depth), depth + 1);
+    }
+  
+    traverse([], 1);
+  
+    return result;
+  }
+  
