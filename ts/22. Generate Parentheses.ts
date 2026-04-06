064f30f0a21e54f26bd3356551bc6d32a82f697b
dDein <dropsy.dice@gmail.com>
2026-04-06 12:37:44 +0200

    22. Generate parenthesis TS

--- ts/22. Generate Parentheses.ts
+++ ts/22. Generate Parentheses.ts
@@ -18,3 +18,45 @@ function generate(
     if (left) generate(str + "(", left - 1, right, res);
     if (right > left) generate(str + ")", left, right - 1, res);
 }
+
+// v2
+function generateParenthesis(n: number): string[] {
+    let results = [];
+
+    const check = (str: string): boolean => {
+        let open: number = 0;
+        let closed: number = 0;
+
+        for (let p of str) {
+            if (p == "(") {
+                open += 1;
+            } else if (p == ")" && closed + 1 <= open) {
+                closed += 1;
+            } else {
+                return false;
+            }
+        }
+
+        return open == closed;
+    }
+
+    const generateParenthesis = (
+        str: string, 
+        n: number
+    ): string | undefined => {
+        if (n == 0) {
+            if (check(str)) {
+                results.push(str);
+            }
+            
+            return;
+        };
+
+        generateParenthesis(str + '(', n - 1);
+        generateParenthesis(str + ')', n - 1);
+    }
+
+    generateParenthesis('', n * 2);
+
+    return results;
+};
