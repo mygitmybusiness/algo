16511f874e96d020030b9d7939ab541443ed64db
dDein <dropsy.dice@gmail.com>
2026-09-01 19:36:22 +0200

    deep clone

---
+++ js/GFE/deepClone.js
@@ -0,0 +1,15 @@
+/**
+ * @template T
+ * @param {T} value
+ * @return {T}
+ */
+export default function deepClone(value) {
+  if (typeof value !== "object" || value === null) return value;
+
+  if (Array.isArray(value)) {
+    return value.map((item) => deepClone(item));
+  }
+  return Object.fromEntries(
+    Object.entries(value).map(([key, val]) => [key, deepClone(val)]),
+  );
+}
